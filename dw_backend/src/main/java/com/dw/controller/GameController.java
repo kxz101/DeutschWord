51d4a8c72cc394c6bb01638f861463da2146edbd
kxz101 <kxz.s.engineer@gmail.com>
2026-06-02 16:49:29 +0200

    Allow deployed frontend origin

--- dw_backend/src/main/java/com/dw/controller/GameController.java
+++ dw_backend/src/main/java/com/dw/controller/GameController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {"http://localhost:5173","https://dw-frontend.onrender.com"})
 @RequestMapping("/api/words")
 public class GameController {
 
